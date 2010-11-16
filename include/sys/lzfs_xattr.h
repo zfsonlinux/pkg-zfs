5b00667d7804c3572fc95349afb3e2bc0cca013f
Rohan Puri <rohan@kqinfotech.com>
2010-11-16 16:39:41 +0530

    function and structure declarations added to lzfs_xattr.h

--- include/sys/lzfs_xattr.h
+++ include/sys/lzfs_xattr.h
@@ -1,5 +1,14 @@
 #ifndef _LZFS_XATTR_H
 #define _LZFS_XATTR_H
 
+extern struct xattr_handler *lzfs_xattr_handlers[];
+ssize_t
+lzfs_listxattr(struct dentry *dentry, char *buffer, size_t size);
+
+extern struct xattr_handler lzfs_xattr_user_handler;
+
+int
+lzfs_xattr_get(struct inode *inode, const char *name,
+                    void *buffer, size_t size);
 #endif /* _LZFS_XATTR_H */
 
