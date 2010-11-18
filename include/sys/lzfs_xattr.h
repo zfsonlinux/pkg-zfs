af68f622fdf9838939384be2a15f713d4da52cf5
Rohan Puri <rohan@kqinfotech.com>
2010-11-18 16:01:31 +0530

    added decl of removexattr to header file

--- include/sys/lzfs_xattr.h
+++ include/sys/lzfs_xattr.h
@@ -2,6 +2,7 @@
 #define _LZFS_XATTR_H
 
 extern struct xattr_handler *lzfs_xattr_handlers[];
+
 ssize_t
 lzfs_listxattr(struct dentry *dentry, char *buffer, size_t size);
 
@@ -10,5 +11,7 @@ extern struct xattr_handler lzfs_xattr_user_handler;
 int
 lzfs_xattr_get(struct inode *inode, const char *name,
                     void *buffer, size_t size);
+int
+lzfs_removexattr(struct dentry *dentry, const char *name);
 #endif /* _LZFS_XATTR_H */
 
