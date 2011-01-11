756b8be6433793cc5ab2b86f46cf7ff73e74809e
deepen <deepen@kqinfotech.com>
2011-01-11 14:56:48 +0530

    fixed: moutpoint property add multiple entries in mtab.

--- include/libzfs_mtab.h
+++ include/libzfs_mtab.h
@@ -5,5 +5,6 @@ extern int zfs_linux_add_entry(char *mountpoint, char *zfs_name,
 		const char *mtab_file, char *mountopt);
 extern int zfs_linux_remove_entry(const char *mountpoint, 
 		const char *zfs_name, const char *mtab_file);
+extern void zfs_linux_remove_slash(char *mntent_mnt_dir);
 
 #endif
