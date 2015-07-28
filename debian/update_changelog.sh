1fb1143a0e7e77051d723156cbace4a3d3fa56e6
Turbo Fredriksson <turbo@bayour.com>
2015-07-28 12:04:05 +0200

    * Version is now directly 'snapshot/debian/$(version)' * Make sure we don't run override_dh_prep-base-deb-files multipe times. * Don't hardcode my info in the changelog. Use envvars FULLNAME and EMAIL. * Don't update the original changelog. Leave that to manual.

--- debian/update_changelog.sh
+++ debian/update_changelog.sh
@@ -2,15 +2,17 @@
 
 version="$1"
 
+if [ -z "${EMAIL}" -o -z "${FULLNAME}" ]; then
+    echo "Missing EMAIL and/or FULLNAME env variable!"
+    exit 1
+fi
+
 cat <<EOF > debian/changelog
 $version
 
   * New upstream GIT version.
 
- -- Turbo Fredriksson <turbo@debian.org>  `date -R`
+ -- ${FULLNAME} <${EMAIL}>  `date -R`
 
 EOF
 cat debian/changelog.in >> debian/changelog
-
-cp debian/changelog debian/changelog.in
-git commit -m 'New upstream GIT version.' debian/changelog.in
